7d631ee1ec4ed9fc59494af8561345556e53dc10
akandil98 <a.kandil.se@gmail.com>
2022-07-16 19:58:41 +0200

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myphotosblocapp/firebase_options.dart';
+import 'package:myphotosblocapp/views/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,35 +11,4 @@ void main() async {
   runApp(
     const App(),
   );
-}
-
-class App extends StatelessWidget {
-  const App({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-    );
-  }
 }
